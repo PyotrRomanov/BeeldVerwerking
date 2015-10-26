85564d0deb06d06c55881473cfc34070245ef777
PyotrRomanov <bor.greve@gmail.com>
2015-10-26 21:15:11 +0100

    Generalized dilate

--- INFOIBV/Form1.cs
+++ INFOIBV/Form1.cs
@@ -22,7 +22,7 @@ namespace INFOIBV
 
         private void LoadImageButton_Click(object sender, EventArgs e)
         {
-           if (openImageDialog.ShowDialog() == DialogResult.OK)             // Open File Dialog
+            if (openImageDialog.ShowDialog() == DialogResult.OK)             // Open File Dialog
             {
                 string file = openImageDialog.FileName;                     // Get the file name
                 imageFileName.Text = file;                                  // Show file name
@@ -32,7 +32,7 @@ namespace INFOIBV
                     InputImage.Size.Height > 512 || InputImage.Size.Width > 512) // Dimension check
                     MessageBox.Show("Error in image dimensions (have to be > 0 and <= 512)");
                 else
-                    pictureBox1.Image = (Image) InputImage;                 // Display input image
+                    pictureBox1.Image = (Image)InputImage;                 // Display input image
             }
         }
 
@@ -42,7 +42,6 @@ namespace INFOIBV
             if (OutputImage != null) OutputImage.Dispose();                 // Reset output image
             OutputImage = new Bitmap(InputImage.Size.Width, InputImage.Size.Height); // Create new output image
             Color[,] Image = new Color[InputImage.Size.Width, InputImage.Size.Height]; // Create array to speed-up operations (Bitmap functions are very slow)
-            Color[,] newImage = new Color[InputImage.Size.Width, InputImage.Size.Height];
 
             // Setup progress bar
             progressBar.Visible = true;
@@ -71,12 +70,13 @@ namespace INFOIBV
                     Color pixelColor = Image[x, y];                         // Get the pixel color at coordinate (x,y)
                     int greyValue = (pixelColor.R + pixelColor.G + pixelColor.B) / 3;
                     Color updatedColor;
-                    if (greyValue > 160)
+                    if (greyValue > 200)
                     {
                         updatedColor = Color.FromArgb(255, 255, 255);
                     }
-                    else {
-                        updatedColor = Color.FromArgb(0, 0, 0); 
+                    else
+                    {
+                        updatedColor = Color.FromArgb(0, 0, 0);
                     }
                     Image[x, y] = updatedColor;                             // Set the new pixel color at coordinate (x,y)
 
@@ -84,35 +84,9 @@ namespace INFOIBV
                 }
 
             }
-            for (int x = 3; x < InputImage.Size.Width-3; x++)
-            {
-                for (int y = 3; y < InputImage.Size.Height-3; y++)
-                {
-                    // Dilate
-                    byte[,] dilateKernel = new byte[5, 5]
-                    {
-                        {0,0,1,0,0},
-                        {0,1,1,1,0},
-                        {1,1,1,1,1},
-                        {0,1,1,1,0},
-                        {0,0,1,0,0}
-                    };
-                    Color newColor = Color.FromArgb(0,0,0);
-                    bool foundColor = false;
-                    for (int i = 0; i < 5 && !foundColor; i++)
-                    {
-                        for (int j = 0; j < 5 && !foundColor; j++)
-                        {;
-                            if (Image[x - 2 + i, y - 2 + j].R * dilateKernel[i, j] > 0)
-                            {
-                                newColor = Color.FromArgb(255, 255, 255);
-                                foundColor = true;
-                            }
-                        }
-                    }
-                    newImage[x, y] = newColor;
-                }
-            }
+
+            Image = Dilate(Image, 2);
+
             //==========================================================================================
 
             // Copy array to output Bitmap
@@ -120,14 +94,14 @@ namespace INFOIBV
             {
                 for (int y = 0; y < InputImage.Size.Height; y++)
                 {
-                    OutputImage.SetPixel(x, y, newImage[x, y]);               // Set the pixel color at coordinate (x,y)
+                    OutputImage.SetPixel(x, y, Image[x, y]);               // Set the pixel color at coordinate (x,y)
                 }
             }
-            
+
             pictureBox2.Image = (Image)OutputImage;                         // Display output image
             progressBar.Visible = false;                                    // Hide progress bar
         }
-        
+
         private void saveButton_Click(object sender, EventArgs e)
         {
             if (OutputImage == null) return;                                // Get out if no output image
@@ -135,5 +109,45 @@ namespace INFOIBV
                 OutputImage.Save(saveImageDialog.FileName);                 // Save the output image
         }
 
+        Color[,] Dilate(Color[,] image, int dilateCount)
+        {
+            Color[,] tempImage = image;
+            Color[,] newImage = new Color[InputImage.Size.Width, InputImage.Size.Height];
+            for (int c = 0; c < dilateCount; c++)
+            {
+                for (int x = 3; x < InputImage.Size.Width - 3; x++)
+                {
+                    for (int y = 3; y < InputImage.Size.Height - 3; y++)
+                    {
+                        // Dilate
+                        byte[,] dilateKernel = new byte[5, 5]
+                    {
+                        {0,0,1,0,0},
+                        {0,1,1,1,0},
+                        {1,1,1,1,1},
+                        {0,1,1,1,0},
+                        {0,0,1,0,0}
+                    };
+                        Color newColor = Color.FromArgb(0, 0, 0);
+                        bool foundColor = false;
+                        for (int i = 0; i < 5 && !foundColor; i++)
+                        {
+                            for (int j = 0; j < 5 && !foundColor; j++)
+                            {
+                                if (tempImage[x - 2 + i, y - 2 + j].R * dilateKernel[i, j] > 0)
+                                {
+                                    newColor = Color.FromArgb(255, 255, 255);
+                                    foundColor = true;
+                                }
+                            }
+                        }
+                        newImage[x, y] = newColor;
+                    }
+                }
+                tempImage = newImage;
+            }
+
+            return newImage;
+        }
     }
 }
