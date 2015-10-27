09746391a66affc34b00dcd74753bbfa857074a1
PyotrRomanov <bor.greve@gmail.com>
2015-10-27 22:20:48 +0100

    Erode, Perimeter en markobjects

--- INFOIBV/Form1.cs
+++ INFOIBV/Form1.cs
@@ -14,6 +14,8 @@ namespace INFOIBV
     {
         private Bitmap InputImage;
         private Bitmap OutputImage;
+        int objectCount;
+        int[,] objectMap;
 
         public INFOIBV()
         {
@@ -42,7 +44,8 @@ namespace INFOIBV
             if (OutputImage != null) OutputImage.Dispose();                 // Reset output image
             OutputImage = new Bitmap(InputImage.Size.Width, InputImage.Size.Height); // Create new output image
             Color[,] Image = new Color[InputImage.Size.Width, InputImage.Size.Height]; // Create array to speed-up operations (Bitmap functions are very slow)
-
+            objectCount = 0;
+            objectMap = new int[InputImage.Size.Width, InputImage.Size.Height];
             // Setup progress bar
             progressBar.Visible = true;
             progressBar.Minimum = 1;
@@ -70,7 +73,7 @@ namespace INFOIBV
                     Color pixelColor = Image[x, y];                         // Get the pixel color at coordinate (x,y)
                     int greyValue = (pixelColor.R + pixelColor.G + pixelColor.B) / 3;
                     Color updatedColor;
-                    if (greyValue > 200)
+                    if (greyValue > 160)
                     {
                         updatedColor = Color.FromArgb(255, 255, 255);
                     }
@@ -84,10 +87,13 @@ namespace INFOIBV
                 }
 
             }
+            
+            for(int a = 0; a < 1; a++ ){
+                Image = Erode(Image);
+            }
+            
 
-
-
-            for (int a = 0; a < 5; a++ )
+            for (int a = 0; a < 10; a++ )
             {
                 Image = Dilate(Image);
             }
@@ -148,5 +154,188 @@ namespace INFOIBV
             }
             return newImage;
         }
+
+        Color[,] Erode(Color[,] image)
+        {
+            Color[,] newImage = new Color[InputImage.Size.Width, InputImage.Size.Height];
+            byte[,] erodeKernel = new byte[3, 3]
+                    {
+                        {1,1,1},
+                        {1,1,1},
+                        {1,1,1}
+                    };
+            
+            for (int x = 1; x < InputImage.Size.Width - 1; x++)
+            {
+                for (int y = 1; y < InputImage.Size.Height - 1; y++)
+                {
+                    int counter = 0;
+
+                    for (int i = 0; i < 3; i++)
+                    {
+                        for (int j = 0; j < 3; j++)
+                        {
+                            if (image[x - 1 + i, y - 1 + j].R > 0 && erodeKernel[i, j] == 1)
+                            {
+                                counter++;
+                            }
+                        }
+                    }
+                    if (counter == 9)
+                    {
+                        newImage[x, y] = Color.FromArgb(255, 255, 255);
+                    }
+                    else {
+                        newImage[x, y] = Color.FromArgb(0, 0, 0);
+                    }
+                }
+            }
+            return newImage;
+        }
+
+        Color[,] MarkObjects(Color[,] image) 
+        {
+            Color[,] newImage = new Color[InputImage.Size.Width, InputImage.Size.Height];
+            for (int x = 1; x < InputImage.Size.Width - 1; x++)
+            {
+                for (int y = 1; y < InputImage.Size.Height - 1; y++)
+                { 
+                    if(image[x,y].R > 0 && objectMap[x,y] == 0){
+                        objectCount++;
+                        objectMap[x, y] = objectCount;
+                        checkNeighbouringPixels(image, x,y,"left");
+                        //newImage[x, y] = Color.FromArgb(objectCount + 1, objectCount + 1, objectCount +1);
+                    }
+                }
+            }
+            return newImage;
+        }
+
+        void checkNeighbouringPixels(Color[,] image, int x, int y, string previouslyChecked) 
+        { 
+            if(previouslyChecked == "left"){
+                if(image[x + 1,y].R > 0 && objectMap[x + 1,y] == 0){
+                    objectMap[x + 1, y] = objectCount;
+                    checkNeighbouringPixels(image, x + 1, y, "left");
+                }
+                if (image[x, y - 1].R > 0 && objectMap[x, y - 1] == 0)
+                {
+                    objectMap[x, y - 1] = objectCount;
+                    checkNeighbouringPixels(image, x, y - 1, "down");
+                }
+                if (image[x, y + 1].R > 0 && objectMap[x, y + 1] == 0)
+                {
+                    objectMap[x, y + 1] = objectCount;
+                    checkNeighbouringPixels(image, x, y + 1, "up");
+                }
+            }
+            if (previouslyChecked == "right")
+            {
+                if (image[x - 1, y].R > 0 && objectMap[x - 1, y] == 0)
+                {
+                    objectMap[x - 1, y] = objectCount;
+                    checkNeighbouringPixels(image, x - 1, y, "right");
+                }
+                if (image[x, y - 1].R > 0 && objectMap[x, y - 1] == 0)
+                {
+                    objectMap[x, y - 1] = objectCount;
+                    checkNeighbouringPixels(image, x, y - 1, "down");
+                }
+                if (image[x, y + 1].R > 0 && objectMap[x, y + 1] == 0)
+                {
+                    objectMap[x, y + 1] = objectCount;
+                    checkNeighbouringPixels(image, x, y + 1, "up");
+                }
+            }
+            if (previouslyChecked == "down")
+            {
+                if (image[x - 1, y].R > 0 && objectMap[x - 1, y] == 0)
+                {
+                    objectMap[x + 1, y] = objectCount;
+                    checkNeighbouringPixels(image, x + 1, y, "right");
+                }
+                if (image[x, y - 1].R > 0 && objectMap[x, y - 1] == 0)
+                {
+                    objectMap[x, y - 1] = objectCount;
+                    checkNeighbouringPixels(image, x, y - 1, "down");
+                }
+                if (image[x + 1, y].R > 0 && objectMap[x, y + 1] == 0)
+                {
+                    objectMap[x + 1, y] = objectCount;
+                    checkNeighbouringPixels(image, x + 1, y, "left");
+                }
+            }
+            if (previouslyChecked == "up")
+            {
+                if (image[x - 1, y].R > 0 && objectMap[x - 1, y] == 0)
+                {
+                    objectMap[x + 1, y] = objectCount;
+                    checkNeighbouringPixels(image, x + 1, y, "right");
+                }
+                if (image[x, y + 1].R > 0 && objectMap[x, y + 1] == 0)
+                {
+                    objectMap[x, y + 1] = objectCount;
+                    checkNeighbouringPixels(image, x, y + 1, "up");
+                }
+                if (image[x + 1, y].R > 0 && objectMap[x, y + 1] == 0)
+                {
+                    objectMap[x + 1, y] = objectCount;
+                    checkNeighbouringPixels(image, x + 1, y, "left");
+                }
+            }
+
+        }
+
+        int perimeter(byte[,] whiteObject)
+        {
+            int count = 0;
+            byte[,] perimeterKernel = new byte[3, 3]
+            {
+                {1,1,1},
+                {1,1,1},
+                {1,1,1}
+            };
+            for (int x = 3; x < whiteObject.Length - 3; x++)
+            {
+                for (int y = 3; y < whiteObject.Length - 3; y++)
+                {
+                    int tempcount = 0;
+                    for (int i = 0; i < 3; i++)
+                    {
+                        for (int j = 0; j < 3; j++)
+                        {
+                            if (whiteObject[x - 2 + i, y - 2 + j] == 1)
+                            {
+                                tempcount++;
+                            }
+                        }
+                    }
+
+                    if (tempcount < 9 && tempcount > 0 && whiteObject[x, y] == 1)
+                    {
+                        count++;
+                    }
+                }
+            }
+            return count;
+        }
+
+        int area(byte[,] whiteObject)
+        {
+            int area = 0;
+            for (int x = 0; x < whiteObject.Length; x++)
+            {
+                for (int y = 0; y < whiteObject.Length; y++)
+                {
+                    if (whiteObject[x, y] == 1)
+                    {
+                        area++;
+                    }
+                }
+            }
+
+            return area;
+        }
+
     }
 }
